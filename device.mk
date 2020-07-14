a9a3e73adbfbf7e282f383cfed26199ffee2b46d
Tom Marshall <tdm.code@gmail.com>
2020-07-14 11:50:07 -0700

    chime: Remove unused vendor.qti.hardware.wifi packages
    * The Lineage framework does not have a dependency on any of these
      packages, since we do not make use of Qualcomm WLAN value-adds.
    
    Change-Id: I8802d3159354a235c25e4d1d3b96cf280dbd5c20

--- device.mk
+++ device.mk
@@ -570,8 +570,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
-    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
