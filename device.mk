709c8369e154574a380b146bbe89ef5ee48bb0af
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    chime: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- device.mk
+++ device.mk
@@ -479,7 +479,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors-service.multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
