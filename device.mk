d4938a4af8d7baa1ed4f839558f671736456acde
Toufu <hamm157famm7423@gmail.com>
2026-09-07 07:01:00 +0000

    chime: Move to bp

--- device.mk
+++ device.mk
@@ -454,11 +454,9 @@ PRODUCT_PACKAGES += \
     init.stnfc.rc \
     init.target.rc \
     fstab.qcom \
+    fstab.qcom.ramdisk \
+    fstab.zram \
     ueventd.qcom.rc 
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram \
 
 # RCS
 PRODUCT_PACKAGES += \
